c9a855d91603cab518e400d6d013381082541493
maverick504 <emilianomangaka@gmail.com>
2019-12-06 01:14:49 -0300

    Update nuxt.config.js

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -8,11 +8,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Drawvania',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description },
+      { hid: 'description', name: 'description', content: '' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { 'http-equiv': 'x-ua-compatible', content: 'ie=edge,chrome=1' }
     ],
